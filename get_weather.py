58913bf8c48167613ec300920cf19ae540f6d646
Max <maxk893@gmail.com>
2018-04-09 21:12:58 -0700

    Renamed several variables for clarity

--- get_weather.py
+++ get_weather.py
@@ -13,14 +13,14 @@ class WeatherData:
         return weather_reply
 
     def get_current_conditions(self):
-        r = requests.get('http://api.wunderground.com/api/%s/conditions/q/%s.json' % (self.api_key, self.zip_code))
-        r = r.json()
-        conditions_reply = [r]
+        conditions_reply = requests.get('http://api.wunderground.com/api/%s/conditions/q/%s.json' % (self.api_key, self.zip_code))
+        conditions_reply = conditions_reply.json()
+        conditions_reply = [conditions_reply]
         return conditions_reply
 
 
     def get_forecast(self):
-        r = requests.get('http://api.wunderground.com/api/%s/forecast/q/%s.json' % (self.api_key, self.zip_code))
-        r = r.json()
-        forecast_reply = [r]
+        forecast_reply = requests.get('http://api.wunderground.com/api/%s/forecast/q/%s.json' % (self.api_key, self.zip_code))
+        forecast_reply = forecast_reply.json()
+        forecast_reply = [forecast_reply]
         return forecast_reply
