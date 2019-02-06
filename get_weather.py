2e70cf89ab7a93097fcd85b5bd86c2bb8fadfacf
Max K <maxk893@gmail.com>
2019-02-06 15:36:17 -0800

    Updated get_weather class to use DarkSky API

--- get_weather.py
+++ get_weather.py
@@ -3,37 +3,49 @@ import datetime
 
 class WeatherData:
 
-    def __init__(self, api_key, zip_code, temp_format):
+    def __init__(self, api_key, latitude, longitude):
         self.api_key = api_key
-        self.zip_code = zip_code
-        self.temp_format = temp_format
-        self.current_conditions_url = 'https://api.openweathermap.org/data/2.5/weather?zip=%s&units=%s&appid=%s' \
-            % (self.zip_code, self.temp_format, self.api_key)
-        self.forecast_url = 'https://api.openweathermap.org/data/2.5/forecast?zip=%s&units=%s&appid=%s' \
-            % (self.zip_code, self.temp_format, self.api_key)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.request_url = 'https://api.darksky.net/forecast/%s/%s,%s?exclude=minutely,flags' \
+            % (self.api_key, self.latitude, self.longitude )
 
     def get_weather(self):
         weather_reply = []
-        weather_reply = [self.get_current_conditions()]
-        #weather_reply.extend(self.get_forecast())
-        return weather_reply
 
-    def get_current_conditions(self):
-        conditions_reply = requests.get(self.current_conditions_url)
-        if (conditions_reply.status_code == 200):
-            conditions_reply = conditions_reply.json()
+        conditions_request = requests.get(self.request_url)
+        if (conditions_request.status_code == 200):
+            conditions_request = conditions_request.json()
 
-            conditions_data = {"temp": conditions_reply["main"]["temp"]}
-            conditions_data["location"] = conditions_reply["name"]
-            conditions_data["icon"] = conditions_reply["weather"][0]["icon"]
+            weather_reply = [self.extract_current_conditions(conditions_request)]
+            weather_reply.append(self.extract_forecast(conditions_request))
+            return weather_reply
 
-            return conditions_data
+    def extract_current_conditions(self, raw_data):
+        conditions_data = {}
+        conditions_data["temp"] = raw_data["currently"]["temperature"]
+        conditions_data["feels_like"] = raw_data["currently"]["apparentTemperature"]
+        conditions_data["icon"] = raw_data["currently"]["icon"]
+        conditions_data["summary"] = raw_data["currently"]["summary"]
+        return conditions_data
 
 
-    """ def get_forecast(self):
-        forecast_reply = requests.get(self.forecast_url)
-        if (forecast_reply.status_code == 200):
-            forecast_reply = forecast_reply.json()
-            
+    def extract_forecast(self, raw_data):
+        conditions_data = {}
+        conditions_data["todays_high"] = raw_data["daily"]["data"][0]["temperatureHigh"]
+        conditions_data["todays_low"] = raw_data["daily"]["data"][0]["temperatureLow"]
+        
+        conditions_data["day1_icon"] = raw_data["daily"]["data"][1]["icon"]
+        conditions_data["day1_high"] = raw_data["daily"]["data"][1]["temperatureHigh"]
+        conditions_data["day1_low"] = raw_data["daily"]["data"][1]["temperatureLow"]
 
-            return [forecast_reply] """
+        conditions_data["day2_icon"] = raw_data["daily"]["data"][2]["icon"]
+        conditions_data["day2_high"] = raw_data["daily"]["data"][2]["temperatureHigh"]
+        conditions_data["day2_low"] = raw_data["daily"]["data"][2]["temperatureLow"]
+
+        conditions_data["day3_icon"] = raw_data["daily"]["data"][3]["icon"]
+        conditions_data["day3_high"] = raw_data["daily"]["data"][3]["temperatureHigh"]
+        conditions_data["day3_low"] = raw_data["daily"]["data"][3]["temperatureLow"]
+
+        return conditions_data
+
